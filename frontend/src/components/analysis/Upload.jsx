1113c7df992bcb569641f05e3a39bf046b467718
Kevin Maldonado <kevin.maldonado@epn.edu.ec>
2026-01-14 17:02:30 -0500

    Vercel config VITE_API_URL_R to conect front with back

--- frontend/src/components/analysis/Upload.jsx
+++ frontend/src/components/analysis/Upload.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import Dashboard from "./Dashboard";
 import Dropzone from "./Dropzone";
 import "./upload.css";
+const API_URL_R = import.meta.env.VITE_API_URL_R;
 
 export default function Upload() {
   const [loading, setLoading] = useState(false);
@@ -23,7 +24,7 @@ export default function Upload() {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/predict",
+        `${API_URL_R}/api/predict`,
         formData,
         { headers: { "Content-Type": "multipart/form-data" } }
       );
